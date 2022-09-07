12b252befb48090f75f7b39b6b1cd6a229789756
du4rt3 <al222010329@gmail.com>
2022-09-07 11:32:22 -0500

    Iniciamos y configuramos el proyecto con react navigation

---
+++ App.js
@@ -0,0 +1,35 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+function HomeScreen() {
+  return (
+    <View style={style.view}>
+      <Text style={style.text}>Hello World</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
